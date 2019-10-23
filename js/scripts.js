a71c0a271cc5e60d021d8a451f821c79fff2426e
Student <example@example.com>
2019-10-23 11:51:18 -0700

    comments out calculator code we aren't using, puts the document ready function around the part we are using.

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,47 @@
-var add = function(number1, number2) {
-  return number1 + number2;
-};
 
-var number1 = parseInt(prompt("Enter a number to add:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
+  var add = function(number1, number2) {
+    return number1 + number2;
+  };
+$(document).ready(function(){
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  var result = add(number1, number2);
+  alert(add(number1, number2));
 
+});
+  //For addition//
 
-//For addition//
+/*
+  var sub = function(number1, number2) {
+    return number1 - number2;
+  };
 
+  var number1 = parseInt(prompt("Enter a number to substract:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = sub(number1, number2);
+  alert(result);
 
-var sub = function(number1, number2) {
-  return number1 -= number2;
-};
+  //For subtraction//
 
-var number1 = parseInt(prompt("Enter a number to substract:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = sub(number1, number2);
-alert(result);
+  var mult = function(number1, number2) {
+    return number1 * number2;
+  };
 
-//For subtraction//
+  var number1 = parseInt(prompt("Enter a number to multiply:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = mult(number1, number2);
+  alert(result);
 
-var mult = function(number1, number2) {
-  return number1 *= number2;
-};
+  //For multiplication//
 
-var number1 = parseInt(prompt("Enter a number to multiply:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = mult(number1, number2);
-alert(result);
+  var div = function(number1, number2) {
+    return number1 / number2;
+  };
 
-//For multiplication//
+  var number1 = parseInt(prompt("Enter a number to divide:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var result = div(number1, number2);
+  alert(result);
 
-var div = function(number1, number2) {
-  return number1 /= number2;
-};
-
-var number1 = parseInt(prompt("Enter a number to divide:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = div(number1, number2);
-alert(result);
-
-
-//For division//
+  //For division//
+*/
